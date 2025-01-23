e2ce4dfad1c4c02dd042318bfe9bc6b590ec275f
dtnguyen0 <davidt.nguyen05@gmail.com>
2025-01-22 19:25:23 -0500

    mistake

--- src/About/About.jsx
+++ src/About/About.jsx
@@ -8,7 +8,7 @@ function About(){
       <div className="about-container">
         <h2>About</h2>
         <div className="about-text">
-          <p>As a passionate programmer with strong interests in AI, Machine Learning, Computer Hardware, Software Development, and Cloud Computing, I want to turn ideas into reality with software solutions that are applicable in any situation.</p>
+          <p>As a passionate programmer with strong interests in AI, Machine Learning, Computer Hardware, Software Development, and Cloud Computing. I want to turn ideas into reality with software solutions that are applicable in any situation.</p>
           <p>My experience spans from full-stack development, cloud computing, and data science which includes technologies and languages such as C/C++, React.js, Python, Pandas, AWS, and many more.</p>
           <p>Outside of everything technical, I've acquired many skills and a variety of interests. I've had the privilege of cutting hair for over 25 students—a skill I picked up as a way to connect with others. I also enjoy listening and discovering new music (fun fact: my main playlist has over 1,400 songs!), investing, staying active through workouts, experiencing different cultures, and playing/watching sports.</p>
         </div>
