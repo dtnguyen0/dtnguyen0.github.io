3072d8d26db436d554a5b27c146994689c3795fb
dtnguyen0 <davidt.nguyen05@gmail.com>
2025-02-01 23:14:25 -0500

    title change

--- src/Experiences/Experiences.jsx
+++ src/Experiences/Experiences.jsx
@@ -8,9 +8,9 @@ function Experiences() {
         <h2>Experiences</h2>
           <div className="experiene-cards">
             <Workcard pic="images/voidtechlogo.jpg" web="https://void-tech-um.github.io/index.html"
-              name="VOID Tech" title="Software Engineer" team="Developer Team" duration="Janurary 2025 - Present" location="Ann Arbor, MI"/>
+              name="VOID Tech" title="Software Developer" team="Developer Team" duration="Janurary 2025 - Present" location="Ann Arbor, MI"/>
             <Workcard pic="images/michiganhackerslogo.jpg" web="https://michhackers.com/"
-              name="Michigan Hackers" title="Software Engineer" team="Advanced Web Development Team" duration="September 2024 - Present" location="Ann Arbor, MI"/>
+              name="Michigan Hackers" title="Software Developer" team="Advanced Web Development Team" duration="September 2024 - Present" location="Ann Arbor, MI"/>
             <Workcard pic="images/n2klogo.jpg" web="https://www.n2k.com/"
               name="N2K" title="IT Intern" team="Talent Development Team" duration="November 2023 – July 2024" location="Fulton, MD"/>
             <Workcard pic="images/sblogo.jpg" web="https://www.montgomeryschoolsmd.org/schools/springbrookhs/"
